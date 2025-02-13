51f602c14806c5dd11cba07313843733f44faa5a
Antoine <ant.delalande@gmail.com>
2025-02-13 17:27:54 +0100

    fix: added polling to docker env

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,10 +7,8 @@ function App() {
     <div className="flex flex-col p-4 gap-4 md:gap-8 h-screen items-center justify-center ">
       <h1 className="text-7xl">Drive Up</h1>
       <p className="text-lg w-1/2 text-center">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-        quibusdam impedit vero deleniti ducimus doloremque laboriosam, tenetur
-        minus neque ipsam laborum eos aliquam dicta eum facere iusto autem quae.
-      </p>
+        sdsdsd
+        </p>
       <div className="flex gap-4">
         <Input placeholder="Type something" className="" />
         <Button size="lg" variant="secondary">
