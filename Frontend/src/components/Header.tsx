2493d85c9da97d294a6db799fb8ef54145f55d59
Antho <rey.anthony1@gmail.com>
2025-03-11 17:19:01 +0100

    fix ok

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center h-[4.5rem] mt-2 mr-2">
-      <img src="/logo2.png" alt="logo" className="w-[16rem]" />
+      <Link to="/">
+        <img src="/logo2.png" alt="logo" className="w-[16rem]" />
+      </Link>
       <Button size="lg" className="rounded-3xl mx-4" variant="outline">
         Se connecter
       </Button>
