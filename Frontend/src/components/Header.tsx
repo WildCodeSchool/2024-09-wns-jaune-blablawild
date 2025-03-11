96d92970dd558a2bdb6322856d477e3d35523092
Antho <rey.anthony1@gmail.com>
2025-03-11 16:21:48 +0100

    feat(header): create Header Component

---
+++ Frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./ui/button";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center h-[4,75rem]">
+      <img src="/logo2.png" alt="logo" className="w-[16rem]" />
+      <Button size="lg" className="rounded-3xl mx-4" variant="outline">
+        Se connecter
+      </Button>
+    </header>
+  );
+}
