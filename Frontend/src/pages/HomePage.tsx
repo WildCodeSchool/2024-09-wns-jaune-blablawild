81b63bda2070899d7bc484ff5e038b82beb2eaf9
Aude Sedillo <aude.sedillo@gmail.com>
2025-03-12 16:38:27 +0100

    QC-26: add carrousel trip home page

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -3,10 +3,11 @@ import img from "../assets/home-background.jpg";
 import Header from "@/components/Header";
 import { FaqSection } from "@/components/faq-section";
 import { InformationSection } from "@/components/Information-section";
+import CarrouselTrip from "@/components/CarrouselTrip";
 
 const HomePage = () => {
   return (
-    <section className="flex flex-col h-screen">
+    <section className="flex flex-col h-screen w-screen gap-6">
       <Header />
       <section className="relative w-full h-[60%] flex justify-center items-center">
         <div className="z-1 w-[60%] md:w-[80%] md:h-[45px]">
@@ -18,12 +19,11 @@ const HomePage = () => {
           className="absolute object-cover top-0 w-full h-full"
         />
       </section>
-      <InformationSection/>
-      <div className="flex-1 flex items-center justify-center">
-        <h1 className="text-4xl font-bold">Drive Up</h1>
+      <InformationSection />
+      <div className="flex items-center justify-center w-full">
+        <CarrouselTrip />
       </div>
-      <FaqSection/>
-    </>
+      <FaqSection />
     </section>
   );
 };
