72f9779c1e061f948a8e4e2ea9974ec10656594c
maximmft <mmaufront@gmail.com>
2025-03-12 11:01:52 +0100

    QC-30 feat: information sections displayed on homepage

--- Frontend/src/components/ui/information-card.tsx
+++ Frontend/src/components/ui/information-card.tsx
@@ -1,16 +1,22 @@
 type InformationCardProps = {
-    title: string;
-    text: string;
-    index: number;
-  };
+  title: string;
+  text: string;
+  index: number;
+};
 
-export const InformationCard = ({ title, text, index }: InformationCardProps) => (
-    <section className="flex flex-col w-9/12 text-center md:text-start">
-      <h1 className={`${index % 2 === 1 ? 'text-forecast' : 'text-accent'} font-semibold text-[16px] lg:text-[18px] mb-3`}>
-        {title}
-      </h1>
-      <p className="text-black text-[12px] lg:text-[14px]">
-        {text}
-      </p>
-    </section>
-  );
+export const InformationCard = ({
+  title,
+  text,
+  index,
+}: InformationCardProps) => (
+  <section className="flex flex-col w-9/12 text-center md:text-start">
+    <h1
+      className={`${
+        index % 2 === 1 ? "text-forecast" : "text-accent"
+      } font-semibold text-[16px] lg:text-[18px] mb-3`}
+    >
+      {title}
+    </h1>
+    <p className="text-black text-[12px] lg:text-[14px]">{text}</p>
+  </section>
+);
