80c40665716de72b4b3737edfa84cea34711605c
Antho <rey.anthony1@gmail.com>
2025-03-12 15:32:07 +0100

    feat(footer): Create footer & Layout

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Layout from "./Layout";
 import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
-import TripCard from "./components/TripCard";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="signup" element={<SignUpPage />} />
-        <Route path="signin" element={<SignInPage />} />
-        <Route path="search-result" element={<TripCard />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="signin" element={<SignInPage />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,26 +1,8 @@
-import SearchBar from "../components/SearchBar/SearchBar";
-import img from "../assets/home-background.jpg";
-import Header from "@/components/Header";
-import { FaqSection } from "@/components/faq-section";
-import { InformationSection } from "@/components/Information-section";
-
 const HomePage = () => {
   return (
-    <section className="flex flex-col">
-      <Header />
-      <section className="relative w-full h-[60vh] flex justify-center items-center">
-        <div className="z-1 w-[70%] md:w-[80%] md:h-[45px]">
-          <SearchBar />
-        </div>
-        <img
-          src={img}
-          alt="Main picture on search trip"
-          className="absolute object-cover top-0 w-full h-full"
-        />
-      </section>
-      <InformationSection />
-      <FaqSection />
-    </section>
+    <div className="flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Drive Up</h1>
+    </div>
   );
 };
 
