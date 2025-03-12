ad361e4ddc955ddf0c964725f31d36e3380bd37c
maximmft <mmaufront@gmail.com>
2025-03-12 11:01:24 +0100

    QC-30 feat: information sections displayed on homepage

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,14 +1,11 @@
 import Header from "@/components/Header";
+import { InformationSection } from "@/components/Information-section";
 
 const HomePage = () => {
   return (
     <>
       <Header />
-      <div className="flex flex-col h-screen w-full">
-        <div className="flex-1 flex items-center justify-center">
-          <h1 className="text-4xl font-bold">Drive Up</h1>
-        </div>
-      </div>
+      <InformationSection/>
     </>
   );
 };
