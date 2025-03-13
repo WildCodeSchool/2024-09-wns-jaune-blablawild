6b5d66b7cb1bcad3cec356325904ed729bbc6bd1
Antoine <ant.delalande@gmail.com>
2025-03-13 10:27:24 +0100

    feat: seed trips

--- Backend/src/config/seed/init.ts
+++ Backend/src/config/seed/init.ts
@@ -2,6 +2,7 @@ import { setDataSource } from "typeorm-extension";
 import { dataSource } from "../db";
 import { User } from "../../entities/user";
 import { faker } from "@faker-js/faker/locale/fr";
+import { Trip } from "../../entities/trip";
 
 const seedDatabase = async () => {
     await dataSource.initialize();
@@ -10,6 +11,7 @@ const seedDatabase = async () => {
 
     try {
         // clear existing data
+        await dataSource.getRepository(Trip).delete({})
         await dataSource.getRepository(User).delete({})
 
         // create users
@@ -26,7 +28,23 @@ const seedDatabase = async () => {
             users.push(await dataSource.getRepository(User).save(user))
         }
 
-        console.log("💪 Database seeding complete!")
+        console.log("💪 Users seeded !")
+
+        // create trips
+        const trips: Trip[] = [];
+        for (let i = 0; i < 20; i++) {
+            const trip = new Trip();
+            trip.departure_city = faker.location.city();
+            trip.arrival_city = faker.location.city();
+            trip.departure_time = faker.date.future();
+            trip.driver = users[i % users.length];
+            trip.price = Math.floor(Math.random() * 40);
+            trips.push(await dataSource.getRepository(Trip).save(trip))
+        }
+
+        console.log("💪 Trips seeded !")
+
+        console.log("🚀 Database seeding complete !")
     } catch (error) {
         console.error("💩 Error seeding database:", error);
     }
