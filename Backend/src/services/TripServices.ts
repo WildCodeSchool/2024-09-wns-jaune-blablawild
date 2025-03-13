0dfe00d09a20e0946ae459280ae679872da2bd60
Aude Sedillo <aude.sedillo@gmail.com>
2025-03-13 15:11:38 +0100

    QC-51: fix popular trip

--- Backend/src/services/TripServices.ts
+++ Backend/src/services/TripServices.ts
@@ -1,9 +1,9 @@
 export const getPopular = () => {
     return [
-        {departure_city: "Marseille", arrival_city: "Toulouse"},
-        {departure_city: "Paris", arrival_city: "Caen"},
-        {departure_city: "Nantes", arrival_city: "Lille"},
-        {departure_city: "Cherbourg", arrival_city: "Toulon"},
-        {departure_city: "Paris", arrival_city: "Toulon"},
+        {departure_city: "Le Tampon", arrival_city: "Villeneuve-d'Ascq"},
+        {departure_city: "Nîmes", arrival_city: "Bordeaux"},
+        {departure_city: "Mulhouse", arrival_city: "Clichy"},
+        {departure_city: "Rennes", arrival_city: "Troyes"},
+        {departure_city: "Créteil", arrival_city: "Courbevoie"},
     ]
 }
