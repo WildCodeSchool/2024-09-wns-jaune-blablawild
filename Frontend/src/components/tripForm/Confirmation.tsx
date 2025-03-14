50205271893d17833dbbbe1e4c87c077e8438aae
maximmft <mmaufront@gmail.com>
2025-03-14 13:07:08 +0100

    fixed confirmation page css

--- Frontend/src/components/tripForm/Confirmation.tsx
+++ Frontend/src/components/tripForm/Confirmation.tsx
@@ -1,13 +1,13 @@
-import { CircleCheck } from "lucide-react";
+import { Check, CircleCheck } from "lucide-react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 export default function Confirmation() {
   return (
     <section className="flex flex-col items-center gap-5">
-      <CircleCheck size={50} />
-      <h1 className="text-3xl text-accent">Trajet publié !</h1>
+      <Check size={30} /> 
+      <h1 className="text-3xl text-accent mb-10">Trajet publié !</h1>
       <Link to="/">
-        <Button variant="outline" className="rounded-full">
+        <Button variant="outline" className="rounded-full px-6 py-5">
           Retour à l'accueil
         </Button>
       </Link>
