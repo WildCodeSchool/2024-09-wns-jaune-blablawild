873082d3c6e758bfcd410cebbecadadb3347cf45
YannLahellec <lahellec.yann@gmail.com>
2025-04-09 15:13:28 +0200

    resolver signup modification

--- Backend/src/resolvers/userResolver.ts
+++ Backend/src/resolvers/userResolver.ts
@@ -1,45 +1,48 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver, Query } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Resolver, Query } from "type-graphql";
 import { User } from "../entities/user";
 import * as argon from "argon2";
 
-
 @InputType()
 class NewUserInput {
-    @Field()
-    firstname!: string
-    @Field()
-    lastname!: string
-    @Field()
-    email!: string
-    @Field()
-    password!: string
+  @Field()
+  firstname!: string;
+  @Field()
+  lastname!: string;
+  @Field()
+  email!: string;
+  @Field()
+  password!: string;
 }
 
 @Resolver(User)
 export class UserResolver {
-    @Query(() => [User])
-    async getUsers() {
-      const users = await User.find();
-      return users;
+  @Query(() => [User])
+  async getUsers() {
+    const users = await User.find();
+    return users;
+  }
+
+  @Mutation(() => String)
+  async signup(@Arg("data") userData: NewUserInput) {
+    try {
+      const existingUser = await User.findOne({
+        where: { email: userData.email },
+      });
+      if (existingUser) {
+        throw new Error("L'utilisateur existe deja");
+      }
+      const hashedPassword = await argon.hash(userData.password);
+      await User.save({
+        firstname: userData.firstname,
+        lastname: userData.lastname,
+        email: userData.email,
+        password: hashedPassword,
+      });
+
+      return JSON.stringify("Utilisateur créé avec success");
+    } catch (error) {
+      console.error("Error creating user:", error);
+      throw new Error();
     }
-
-    @Mutation(() => String)
-    async signup(
-        @Arg("data") userData: NewUserInput,
-    ) {
-        // const existingUser = await User.findOne({ where: { email: userData.email } });
-        // if (existingUser) {
-        //     throw new Error("L'utilisateur existe deja");
-        // }
-       const hashedPassword = await argon.hash(userData.password)
-       const user = await User.save({
-           firstname: userData.firstname,
-           lastname: userData.lastname,
-           email: userData.email,
-           password: hashedPassword
-       })
-
-     return JSON.stringify("Utilisateur créé avec success") 
-    }
-
+  }
 }
