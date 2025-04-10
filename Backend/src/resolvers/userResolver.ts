3289920eac0f6123efd4fb0352ee70ba5b515b9d
YannLahellec <lahellec.yann@gmail.com>
2025-04-10 16:33:01 +0200

    user auth done

--- Backend/src/resolvers/userResolver.ts
+++ Backend/src/resolvers/userResolver.ts
@@ -3,11 +3,19 @@ import { User } from "../entities/user";
 import * as argon from "argon2";
 
 @InputType()
- export class NewUserInput {
+export class NewUserInput {
   @Field()
   firstname!: string;
   @Field()
   lastname!: string;
+  @Field()
+  email!: string;
+  @Field()
+  password!: string;
+}
+
+@InputType()
+export class LoginInput {
   @Field()
   email!: string;
   @Field()
@@ -42,6 +50,35 @@ export class UserResolver {
       return JSON.stringify("Utilisateur créé avec success");
     } catch (error) {
       console.error("Error creating user:", error);
+      throw error;
+    }
+  }
+
+  @Mutation(() => String)
+  async login(@Arg("data") loginData: LoginInput) {
+    try {
+      const user = await User.findOne({
+        where: { email: loginData.email },
+      });
+
+      if (!user) throw new Error("Utilisateur non trouvé");
+
+      const isPasswordValid = await argon.verify(
+        user.password,
+        loginData.password
+      );
+
+      if (!isPasswordValid) throw new Error("Mot de passe incorrect");
+
+      const userData = {
+        id: user.id,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+      };
+
+      return JSON.stringify(userData);
+    } catch (error) {
       throw error;
     }
   }
