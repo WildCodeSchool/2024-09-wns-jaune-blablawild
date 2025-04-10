fbe05c1fc3f1c42723555109741cda9cfee0defa
Antoine <ant.delalande@gmail.com>
2025-04-10 12:29:53 +0200

    test: implement getUserById

--- Backend/src/resolvers/userResolver.ts
+++ Backend/src/resolvers/userResolver.ts
@@ -22,12 +22,12 @@ export class UserResolver {
     return users;
   }
 
-    @Query(() => User)
-    async getUserById(@Arg("id") id: string) {
-        const user = await User.findOneBy({ id })
-        if (!user) throw new Error("User not found")
-        return user
-    }
+  @Query(() => User)
+  async getUserById(@Arg("id") id: string) {
+    const user = await User.findOneBy({ id })
+    if (!user) throw new Error("User not found")
+    return user
+  }
 
   @Mutation(() => String)
   async signup(@Arg("data") userData: NewUserInput) {
