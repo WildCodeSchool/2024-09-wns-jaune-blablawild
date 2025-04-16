4b5ac3647bf8e4f8838911cd06c0fb92440f7865
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2025-04-16 15:03:09 +0200

    add mutation to update Password + test

--- Backend/src/resolvers/userResolver.ts
+++ Backend/src/resolvers/userResolver.ts
@@ -1,4 +1,13 @@
-import { Arg, Field, InputType, Mutation, Resolver, Query, Ctx } from "type-graphql";
+import {
+  Arg,
+  Field,
+  InputType,
+  Mutation,
+  Resolver,
+  Query,
+  Ctx,
+  Authorized,
+} from "type-graphql";
 import { User } from "../entities/user";
 import * as argon from "argon2";
 import { generateToken } from "../services/UserServices";
@@ -24,6 +33,16 @@ export class LoginInput {
   password!: string;
 }
 
+@InputType()
+export class UpdatePasswordInput {
+  @Field()
+  oldPassword!: string;
+  @Field()
+  newPassword!: string;
+  @Field()
+  confirmPassword!: string;
+}
+
 @Resolver(User)
 export class UserResolver {
   @Query(() => [User])
@@ -34,13 +53,16 @@ export class UserResolver {
 
   @Query(() => User)
   async getUserById(@Arg("id") id: string) {
-    const user = await User.findOneBy({ id })
-    if (!user) throw new Error("User not found")
-    return user
+    const user = await User.findOneBy({ id });
+    if (!user) throw new Error("User not found");
+    return user;
   }
 
   @Mutation(() => String)
-  async signup(@Arg("data") userData: NewUserInput, @Ctx() { res }: { res: Response }) {
+  async signup(
+    @Arg("data") userData: NewUserInput,
+    @Ctx() { res }: { res: Response }
+  ) {
     try {
       const existingUser = await User.findOne({
         where: { email: userData.email },
@@ -49,23 +71,22 @@ export class UserResolver {
         throw new Error("L'utilisateur existe deja");
       }
 
-      const user = new User()
-      Object.assign(user, userData)
+      const user = new User();
+      Object.assign(user, userData);
 
       const hashedPassword = await argon.hash(userData.password);
-      user.password = hashedPassword
+      user.password = hashedPassword;
 
-      await user.save()
+      await user.save();
 
-      generateToken(user.id, res)
+      generateToken(user.id, res);
 
-      return JSON.stringify({ 
-        id: user.id, 
-        firstname: user.firstname, 
-        lastname: user.lastname, 
-        email: user.email 
+      return JSON.stringify({
+        id: user.id,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
       });
-
     } catch (error) {
       console.error("Error creating user:", error);
       throw error;
@@ -73,7 +94,10 @@ export class UserResolver {
   }
 
   @Mutation(() => String)
-  async login(@Arg("data") loginData: LoginInput, @Ctx() { res }: { res: Response }) {
+  async login(
+    @Arg("data") loginData: LoginInput,
+    @Ctx() { res }: { res: Response }
+  ) {
     try {
       const user = await User.findOne({
         where: { email: loginData.email },
@@ -88,7 +112,7 @@ export class UserResolver {
 
       if (!isPasswordValid) throw new Error("Mot de passe incorrect");
 
-      generateToken(user.id, res)
+      generateToken(user.id, res);
 
       const userData = {
         id: user.id,
@@ -101,5 +125,22 @@ export class UserResolver {
     } catch (error) {
       throw error;
     }
+  }
+
+  @Authorized()
+  @Mutation(() => String)
+  async updatePassword(
+    @Arg("data") data: UpdatePasswordInput,
+    @Ctx() context: { user: User }
+  ) {
+    const user = await User.findOneBy({ id: context.user.id });
+    if (!user) throw new Error("Utilisateur non trouvé");
+    const isPasswordValid = await argon.verify(user.password, data.oldPassword);
+    if (!isPasswordValid)
+      throw new Error("L'ancien mot de passe est incorrect");
+    const hashedPassword = await argon.hash(data.newPassword);
+    user.password = hashedPassword;
+    await user.save();
+    return JSON.stringify("Le mot de passe a bien été modifié");
   }
 }
