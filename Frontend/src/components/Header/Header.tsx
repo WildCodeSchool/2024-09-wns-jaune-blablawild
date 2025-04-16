ec047fcae89db009889c770e7b95dd905e6d24a3
Maxime Maufront <mmaufront@gmail.com>
2025-04-16 15:42:00 +0200

    QC-83 fixed css navbar "publish button" + fixed carrousel to avoid spacing right in homepage (#67)
    * fixed css
    
    * fixed carrousel to avoid bug in homepage (spacing right on screen)

--- Frontend/src/components/Header/Header.tsx
+++ Frontend/src/components/Header/Header.tsx
@@ -5,18 +5,34 @@ import NavBar from "../NavBar/NavBar";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center h-[3rem] mt-1 mr-3 border-b border-solid">
-      <Link to="/">
-        <img src="/logo2.png" alt="logo" className="w-[10rem]" />
-      </Link>
-      <div className="flex gap-2 items-center">
-        <Link to="/tripform">
-          <Button size="lg" className="rounded-3xl" variant="outline">
-            <Plus />
-            <p className="md:block hidden">Ajouter un trajet</p>
+    <header className="flex justify-between items-center h-[3rem] border-b border-solid px-3 md:px-0">
+      <div className="md:hidden">
+        <NavBar />
+      </div>
+
+      <div>
+        <Link to="/">
+          <img src="/logo2.png" alt="logo" className="w-[10rem]" />
+        </Link>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Link to="/tripform" className="md:block hidden">
+          <Button size="lg" className="rounded-3xl hover:no-underline hover:opacity-80" variant="link">
+            <p className="text-foreground">Publier un trajet</p>
           </Button>
         </Link>
-        <NavBar />
+        
+        <Link
+          to="/tripform"
+          className="md:hidden border-2 border-foreground rounded-full p-1"
+        >
+          <Plus strokeWidth={2} size={18} />
+        </Link>
+
+        <div className="hidden md:block md:mr-3">
+          <NavBar />
+        </div>
       </div>
     </header>
   );
