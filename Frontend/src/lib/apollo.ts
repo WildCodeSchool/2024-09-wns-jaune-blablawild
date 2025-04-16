929784beee9ba80df5bdb59667d6de8c76beafc8
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2025-04-16 10:41:54 +0200

    rm cached

--- Frontend/src/lib/apollo.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:7100/api",
-  cache: new InMemoryCache(),
-});
-
-export default client;
--- Frontend/src/utils/FormatTime.tsx
+++
@@ -1,6 +0,0 @@
-export function formatTime(dateString: string, locale: string) {
-    return new Date(dateString).toLocaleString(locale, {
-      hour: "2-digit",
-      minute: "2-digit"
-    });
-  }
