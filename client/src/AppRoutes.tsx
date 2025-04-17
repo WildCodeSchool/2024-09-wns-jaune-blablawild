f38bc501fb3a21f3fce68bf87b42ab0705692d97
Antho <rey.anthony1@gmail.com>
2025-04-17 17:37:09 +0200

    add context

---
+++ client/src/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "./pages/HomePage";
+import Page404 from "./pages/Other/Page404";
+import SearchTrip from "./pages/SearchTrip/SearchTrip";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import TripForm from "./pages/TripForm";
+import UserJourneys from "./pages/UserJourneys";
+import UserProfilePage from "./pages/UserProfile/UserProfilePage";
+
+export default function AppRoutes() {
+  const location = useLocation();
+  const routesWithFooter = ["/"];
+  const showFooter = routesWithFooter.includes(location.pathname);
+
+  return (
+    <Layout showFooter={showFooter}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="search-result" element={<SearchTrip />} />
+        <Route path="user/:id" element={<UserProfilePage />} />
+        <Route path="tripform" element={<TripForm />} />
+        <Route path="userjourneys" element={<UserJourneys />} />
+        <Route path="404" element={<Page404 />} />
+      </Routes>
+    </Layout>
+  );
+}
---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { useUserStore } from "@/store/useUserStore";
+import { createContext, useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated } = useUserStore();
+  const location = useLocation();
+
+  const publicRoutes = ["/", "/signin", "/signup", "/search-result", "/404"];
+
+  const isPublicRoute = publicRoutes.some(
+    (route) =>
+      location.pathname === route || location.pathname.startsWith(`${route}?`)
+  );
+
+  if (!isAuthenticated && !isPublicRoute) {
+    return <Navigate to="/404" state={{ from: location }} replace />;
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error(
+      "useAuth doit être utilisé à l'intérieur d'un AuthProvider"
+    );
+  }
+  return context;
+}
