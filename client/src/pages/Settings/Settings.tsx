68a47df79cb314b61ea83310239498f2b88890a2
maximmft <mmaufront@gmail.com>
2025-04-18 15:58:34 +0200

    background image settings page

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -4,6 +4,7 @@ import { getUrlParams } from "@/utils";
 import NavigationSettings from "./components/NavigationSettings";
 import { ChevronLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import van_image from "@/assets/van-image-trip-form.png";
 
 export default function Settings() {
   const navigate = useNavigate();
@@ -56,34 +57,42 @@ export default function Settings() {
   ];
 
   return (
-    <section className="flex justify-center items-center w-full h-full">
-      <section
-        className={`${
-          activeSettings ? "hidden" : "flex"
-        } h-full w-full md:w-1/2 flex-col gap-4`}
-      >
-        <NavigationSettings
-          settingsGroups={settingsGroups}
-          setActiveSettings={setActiveSettings}
-          activeSettings={activeSettings}
+    <section className="flex min-h-screen">
+      <div className="w-[15%] hidden lg:block">
+        <img src={van_image} alt="van" className="h-full w-full object-cover" />
+      </div>
+      <div className="w-full lg:w-[70%] bg-white md:p-8 overflow-y-auto">
+        {!activeSettings ? (
+          <section className="h-full w-full flex flex-col gap-4">
+            <h1 className="text-2xl my-4 px-6 md:px-0 md:text-3xl md:mb-8">Paramètres</h1>
+            <NavigationSettings
+              settingsGroups={settingsGroups}
+              setActiveSettings={setActiveSettings}
+              activeSettings={activeSettings}
+            />
+          </section>
+        ) : (
+          <section className="flex flex-col items-start justify-start gap-8 p-6">
+            <div
+              className="flex items-center justify-start w-full cursor-pointer"
+              onClick={() => {
+                navigate("/settings");
+                setActiveSettings("");
+              }}
+            >
+              <ChevronLeft className="w-8 h-8" />
+            </div>
+            {activeSettings === "updatePassword" && <UpdatePassword />}
+          </section>
+        )}
+      </div>
+      <div className="w-[15%] hidden lg:block">
+        <img
+          src={van_image}
+          alt="van"
+          className="h-full w-full object-cover object-[75%_center]"
         />
-      </section>
-      {activeSettings && (
-        <section
-          className={`md:w-1/2 w-full flex flex-col items-center justify-center gap-8 p-6`}
-        >
-          <div
-            className="flex items-center justify-start w-full cursor-pointer"
-            onClick={() => {
-              navigate("/settings");
-              setActiveSettings("");
-            }}
-          >
-            <ChevronLeft className="w-8 h-8" />
-          </div>
-          {activeSettings === "updatePassword" && <UpdatePassword />}
-        </section>
-      )}
+      </div>
     </section>
   );
 }
