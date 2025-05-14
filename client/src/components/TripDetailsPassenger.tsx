a0e33ac5d9fc65e3f77b70474b5f578bd64d8972
maximmft <mmaufront@gmail.com>
2025-05-14 10:49:22 +0200

    responsive done

--- client/src/components/TripDetailsPassenger.tsx
+++ client/src/components/TripDetailsPassenger.tsx
@@ -28,8 +28,8 @@ export const TripDetailsPassenger = ({
   if (!passengers || passengers.length === 0) return null;
 
   return (
-    <div className="bg-background w-[583px] border-[#E5E5E5] border rounded-lg px-6 py-4 mb-6">
-      <div className="grid grid-cols-2 gap-4">
+    <div className="bg-background w-full sm:w-11/12 md:w-full lg:w-[480px] xl:w-[583px] border-[#E5E5E5] border rounded-lg px-4 md:px-6 py-3 md:py-4 mb-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 md:gap-4">
         {passengers.map((passenger, index) => {
           if (!passenger.profile) return null;
 
@@ -37,17 +37,17 @@ export const TripDetailsPassenger = ({
             <div
               onClick={() => handleNavigateProfile(passenger.profile!.id)}
               key={passenger.profile.id || index}
-              className="flex items-center p-3 cursor-pointer"
+              className="flex items-center p-2 md:p-3 cursor-pointer"
             >
               <img
                 src={passenger.profile.image || "/placeholder-portrait.png"}
                 alt={passenger.profile.user?.firstname || ""}
-                className="w-10 h-10 rounded-full object-cover mr-3"
+                className="w-8 h-8 md:w-10 md:h-10 rounded-full object-cover mr-2 md:mr-3"
               />
-              <p className="text-forecast">
+              <p className="text-forecast text-sm md:text-base">
                 {passenger.profile.user?.firstname || ""}
               </p>
-                <ChevronRight strokeWidth={1} color="black"/>
+              <ChevronRight strokeWidth={1} color="black" className="ml-auto" />
             </div>
           );
         })}
