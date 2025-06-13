0181e2de2c3b8cb43709b2375510a7f9bfd7629e
YannLahellec <lahellec.yann@gmail.com>
2025-06-13 11:05:45 +0200

    done

--- client/src/components/InteractiveMap.tsx
+++ client/src/components/InteractiveMap.tsx
@@ -80,7 +80,7 @@ export const DisplayMap = ({ departureCoordinates, arrivalCoordinates }: Display
         zoomControl={true}
       >
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+          url={`https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png?api_key=${import.meta.env.VITE_STADIA_MAP_API_KEY}`}
           minZoom={0}
           maxZoom={20}
           attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
